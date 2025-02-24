3b838465de98dc5a23958966b5f3e8e20417ff5d
Kevin-webdesign <uzamurerakevin01@gmail.com>
2025-02-24 05:17:40 +0200

    Initial commit for Ever-Retreat web app

---
+++ script.js
@@ -0,0 +1,21 @@
+
+ // Initialize Date Range Picker
+ $(document).ready(function() {
+    $('#daterange').daterangepicker({
+        opens: 'left',
+        autoApply: true,
+        locale: {
+            format: 'YYYY-MM-DD',
+            separator: ' - ',
+            applyLabel: 'Apply',
+            cancelLabel: 'Cancel',
+            fromLabel: 'From',
+            toLabel: 'To',
+            customRangeLabel: 'Custom',
+            weekLabel: 'W',
+            daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+            monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            firstDay: 1
+        }
+    });
+});
